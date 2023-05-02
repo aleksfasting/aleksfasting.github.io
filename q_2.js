3687a9d18fbe404ba70a2d0cee6f1ecbc671fed8
aleksfasting <aleks.fasting@gmail.com>
2023-05-02 02:18:33 +0200

    urdu fix

--- q_2.js
+++ q_2.js
@@ -3,7 +3,7 @@ button = document.querySelector("#button")
 button.addEventListener('click', submit)
 
 function submit() {
-    if (input.value.toLowerCase() == 'mein shiza ko pesant karta hein') {
+    if (input.value.toLowerCase() == 'mein shiza ko pasand karta hoon') {
         document.location.replace("finish_2.html")
     }
 }
